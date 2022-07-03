7ecec1db332b43bcfcad80d483a9d7802ebbae6d
김창희 <45018724+PiperChang@users.noreply.github.com>
2022-07-03 23:27:34 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,21 @@
+def solution(s):
+    l = int(len(s) / 2) + 1
+    answer = len(s)
+    for i in range(1,l) : # 자르는 기준 
+        string = s[:i] # i 길이의 문자열
+        repeat = 1
+        minlen = ''
+        idx = 0
+        for idx in range(i,len(s)+i,i) :
+            if string == s[idx:idx+i] :
+                repeat += 1
+            else :
+                if repeat > 1 :
+                    minlen += str(repeat) + string
+                else :
+                    minlen += string
+                string = s[idx:idx+i]
+                repeat = 1
+        # print(minlen)
+        answer = min(answer, len(minlen)) 
+    return answer
