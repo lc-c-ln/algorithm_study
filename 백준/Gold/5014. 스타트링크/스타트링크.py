026b7cb865119a93aa850eb755ac3e9bc20d8190
김창희 <45018724+PiperChang@users.noreply.github.com>
2022-07-24 14:59:33 +0900

    [Gold V] Title: 스타트링크, Time: 852 ms, Memory: 70920 KB -BaekjoonHub

--- 백준/Gold/5014. 스타트링크/스타트링크.py
+++ 백준/Gold/5014. 스타트링크/스타트링크.py
@@ -2,49 +2,23 @@ from collections import deque
 
 F, S, G, U, D = list(map(int,input().split()))
 
-visited = [0 for i in range(F+1)]
-count = [0 for i in range(F+1)]
+visited = [-1 for i in range(F+1)]
+q = deque([S])
+visited[S] = 0
+ans = -1
 
-def bfs(v) :
-  q = deque([v])
-  visited[v] = 1
-  while q :
-    v = q.popleft()
-    if v == G:
-      return count[G]
-    if v + U <= F and visited[v + U] == 0 :
-      visited[v + U] = 1
-      count[v+U] = count[v] + 1
-      q.append(v + U)
-    if v - D > 0 and visited[v - D] == 0 :
-      visited[v- D] = 1
-      count[v- D] = count[v] + 1
-      q.append(v- D)
-  if count[G] == 0 :
-    return "use the stairs"
-
-print(bfs(S))
-      
-
-  
-
-# ans = -1
-
-
-# while q :
-#   v = q.popleft()
-#   if pos == G :
-#     # ans = count[pos]
-#     break;
-#   if pos + U <= F and visited[pos + U] == 0 :
-#     visited[pos+U] = 1
-#     q.append(pos + U)
-#     count[pos+U] = count[pos] + 1
-#   if pos - D > 0 and visited[pos - D] == 0 :
-#     visited[pos-D] = 1
-#     q.append(pos - D)
-#     count[pos - D] = count[pos] + 1
-# if count[G] == 0 :
-#   print("use the stairs")
-# else :
-#   print(count[G])
+while q :
+  n = q.popleft()
+  if n == G :
+    ans = visited[n]
+    break;
+  if n+U <= F and visited[n+U] == -1 :
+    q.append(n+U)    
+    visited[n+U] = visited[n]+1
+  if n-D >= 1 and visited[n-D] == -1 :
+    q.append(n-D)    
+    visited[n-D] = visited[n]+1
+if ans == -1 :
+  print("use the stairs")
+else :
+  print(ans)
